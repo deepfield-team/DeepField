87ebb64e9356bcf06c10347d8f19a4457ae1fb34
Musheg <musheg901@mail.ru>
2024-06-28 03:04:53 +0300

    make faults subscriptable

--- deepfield/field/faults.py
+++ deepfield/field/faults.py
@@ -4,29 +4,50 @@ import pandas as pd
 from .base_component import BaseComponent
 from .parse_utils import parse_eclipse_keyword
 
+
 class Faults(BaseComponent):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self._data = {}
+        self._names = []
 
     def __getitem__(self, key):
-        return super().__getitem__(key)
+        fault = _Fault(self, key)
+        return fault
 
-    def __setitem__(self, key, value):
-        return super().__setitem__(key, value)
-    
-    def __delitem__(self, key):
-        return super().__delitem__(key)
-    
-    def __contains__(self, x):
-        return super().__contains__(x)
-    
+    @property
+    def names(self):
+        if hasattr(self, 'FAULTS'):
+            self._names = self._data['FAULTS']['NAME'].unique()
+        return self._names
+
     def _read_buffer(self, buffer, attr, **kwargs):
-        columns = ['NAME', 'IX1', 'IX2', 'IY1', 'IY2', 'IZ1', 'IZ2', 'FACE']
-        column_types = {
-        'text': [columns[0], columns[-1]],
-        'int': columns[1:-1]
-        }
-        table = parse_eclipse_keyword(buffer, columns, column_types) #read_table(buffer, TABLE_INFO[attr], dtype)
-        setattr(self, attr, table)
+        if attr == 'FAULTS':
+           columns = ['NAME', 'IX1', 'IX2', 'IY1', 'IY2', 'IZ1', 'IZ2', 'FACE']
+           column_types = {
+                'text': [columns[0], columns[-1]],
+                'int': columns[1:-1]
+           }
+           table = parse_eclipse_keyword(buffer, columns, column_types)
+           self._data['FAULTS'] = table
+        elif attr == 'MULTFLT':
+            columns = ['NAME', 'MULT']
+            column_types = {
+                'text': [columns[0], columns[-1]],
+                'int': columns[1:-1]
+            }
+            table = parse_eclipse_keyword(buffer, columns, column_types)
+            self._data['MULTFLT'] = table
         return self
+
+class _Fault:
+    def __init__(self, faults, key):
+        self._faults = faults
+        self._key = key
+
+    def __getattr__(self, attr):
+        if attr.upper() in self._faults._data:
+            df = self._faults._data[attr.upper()]
+            return df[df['NAME'] == self._key]
+        raise AttributeError("{} has no attribute {}".format(self._key, attr))
