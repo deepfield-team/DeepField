cedd29fabbc3d58e47629fdeb3c9dfb7fce69481
Musheg <musheg901@mail.ru>
2025-02-18 03:19:00 +0300

    linter fix

--- deepfield/tests/conftest.py
+++ deepfield/tests/conftest.py
@@ -2,10 +2,12 @@
 import pytest
 
 def pytest_addoption(parser):
+    """Commandline option for assigning tNavigator tutorials path."""
     parser.addoption("--path_to_tnav_tutorials",
                         action="store",
                         default="./tNav_tutorials")
 
 @pytest.fixture(scope="module")
 def path_to_tnav_tutorials(request):
+    """Returns tNavigator tutorials path"""
     return request.config.getoption("--path_to_tnav_tutorials")
